68678a8b5bcd16136a5cab78ca48fe7279e2dcf6
knightofren2588 <knightofren2488@gmail.com>
2024-05-14 16:27:58 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+//outcomes
+    //rock destroys scissors
+    //scissors cut paper
+    // paper covers rock
+//tie
+    //its a draw
+// 4 Game parts
+    //user choice
+    //computer choice
+    //compare both and determine winner
+    //start program and display results
+
+var userInput = 'rock';
+var userInput = 'paper';
+var userInput = 'scissors';
+
+var getUserChoice = function(userInput){
+} 
